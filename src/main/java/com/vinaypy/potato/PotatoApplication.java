85341d53e1805cb2e5666149fd038584fb0c8ff0
Vinay <vinay.py@gmail.com>
2021-04-08 14:16:24 -0400

    First commit

---
+++ src/main/java/com/vinaypy/potato/PotatoApplication.java
@@ -0,0 +1,13 @@
+package com.vinaypy.potato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinaypy/potato/PotatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinaypy.potato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
